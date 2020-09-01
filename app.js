65ba3ad708532a6c2b215ee04b6f54e37739aa67
Tre' Zareck <trezar001@gmail.com>
2020-09-01 16:00:51 -0400

    electron start

--- app.js
+++ app.js
@@ -1,1 +1,18 @@
-let electron = require('electron');
+const electron = require('electron');
+
+const { app, BrowserWindow } = electron;
+
+let mainWindow;
+
+//listee for app to be ready
+app.on('ready',function(){
+
+    mainWindow = new BrowserWindow({
+        width: 800,
+        height: 600,
+        webPreferences: {
+          nodeIntegration: true
+        }
+    })
+})
+
