ad6b3c46c80602d3af0c76c4ee0080c0e5255857
Tre' Zareck <trezar001@gmail.com>
2020-09-10 15:23:49 -0400

    basic three col display

--- action.js
+++ action.js
@@ -1,6 +1,12 @@
 
-let button = document.getElementById('btn')
+components = document.querySelectorAll('.component')
 
-button.onclick(e => {
-    
-})
+components.forEach(component =>{
+    component.onclick = (e =>{
+        let description = document.getElementById('component-description')
+        let output = document.getElementById('component-output')
+
+        description.innerHTML = component.innerHTML
+        output.innerHTML = component.innerHTML + ' output'
+    })
+}) 
